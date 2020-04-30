90cb6431f1bbfa75c3c71daff92ab59149bb3a84
tamarazu <tamarazulaika.u@gmail.com>
2020-04-30 19:51:41 +0700

    finalisasi test-unsircle

--- test-unsircle/src/components/NavbarPanel.js
+++ test-unsircle/src/components/NavbarPanel.js
@@ -10,9 +10,11 @@ function NavbarPanel () {
 
     return (
     <Navbar bg="light d-flex justify-content-between">
-        <Navbar.Brand href="#home">Unsircle Test</Navbar.Brand>
+        <Navbar.Brand href="#home"  style={{paddingLeft: '20px'}}>
+                <img src="https://i0.wp.com/unsircle.id/wp-content/uploads/2020/01/Unsircle-logo-png.png?fit=1079%2C300&ssl=1" alt="RegisterPicture" style={{width: '170px'}}/>
+        </Navbar.Brand>
         <Link 
-            style={{padding: "5", fontSize: '20px'}}
+            style={{padding: "5", fontSize: '20px', color: 'black', paddingRight: '15px'}}
             onClick={(event) => logout(event)}
         >Logout</Link>
     </Navbar>
--- test-unsircle/src/views/Home.js
+++ test-unsircle/src/views/Home.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 
 import NavbarPanel from '../components/NavbarPanel'
 import homePicture from '../assets/homePicture.svg'
+import Loading from '../components/Loading'
 
 export default function Home () {
     const history = useHistory()
 
+    const loading = useSelector(state => state.reducers.loading)
+
     if(!localStorage.token) history.push('/login')
+    if (loading) return <Loading/>
     return (
         <div>
             <NavbarPanel/>
-            <div>
+            <div className="container">
                 <Row style={{justifyContent: "center"}}>
                     <Col >
-                        <h1>Welcome to Unsircle</h1>
+                        <h1 className="display-3" style={{textAlign: 'center', padding: '20px'}}>Welcome to Unsircle</h1>
                         <img src={homePicture} alt="controlPicture"/>
                     </Col>
                 </Row>
